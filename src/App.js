a56fa8f8cb7d3a8eee3dc8bcec7881a891e8e787
temmietope <temmieayodele@gmail.com>
2020-11-19 10:00:46 +0100

    setting i[ apollo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import BookList from './components/BookList'
 
-function App() {
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/GraphQL',
+  cache: new InMemoryCache(),
+})
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>My Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/BookList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`
+
+const BookList = () => {
+  const { loading, error, data } = useQuery(getBooksQuery)
+  console.log(data)
+  if (loading) return <p>Loading</p>
+  if (error) return <p>Ooops! Something went wrong</p>
+  return (
+    <div>
+      <ul id="book-list">
+        <li>Book name</li>
+      </ul>
+    </div>
+  )
+}
+
+export default BookList
